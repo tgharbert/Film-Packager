bf91d6bff820872178b5113e8103e8a7c8495934
Thomas Harbert <tgharbert3@gmail.com>
2024-10-02 20:44:46 -0500

    redirect written for login

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -1,16 +1,49 @@
 package routes
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
-func Homepage(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/index.html", "templates/sidebar.html", "templates/docList.html", "templates/fileUpload.html", "templates/login.html"))
+var auth = false
+func setAuthTrue () {
+	auth = true
+}
 
+func Homepage(w http.ResponseWriter, r *http.Request) {
+	// TEMP AUTH REDIRECT
+	// redirect to false?
+	if !auth {
+		http.Redirect(w, r, "/login/", http.StatusFound)
+	}
+	tmpl := template.Must(template.ParseFiles("templates/index.html",
+	"templates/docList.html", "templates/fileupload.html", "templates/sidebar.html",
+	))
 	// REPLACE THE NIL WITH DATA
 	err := tmpl.Execute(w, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/loginFORM.html"))
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error executing the fucking template")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func PostLogin(w http.ResponseWriter, r *http.Request) {
+	// Check if the request is an HTMX request
+	if r.Header.Get("HX-Request") == "true" {
+		setAuthTrue()
+		// HTMX request, use HX-Redirect to tell HTMX to redirect
+		w.Header().Set("HX-Redirect", "/")
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusFound)
 }
--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", routes.Homepage)
+	http.HandleFunc("/login/", routes.Login)
+	http.HandleFunc("/post-login/", routes.PostLogin)
 	log.Print("Listening on port 3000...")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
