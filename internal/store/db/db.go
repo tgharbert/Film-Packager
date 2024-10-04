a39895016d879114e50cf0b8f6e6583880a026a7
Thomas Harbert <tgharbert3@gmail.com>
2024-10-04 16:24:03 -0500

    storing new users, cleanedup

--- internal/store/db/db.go
+++ internal/store/db/db.go
@@ -31,51 +31,30 @@ func Connect() *pgx.Conn {
 
 func CreateUser(c *pgx.Conn, name string, email string, password string) (User, error) {
 	query := `INSERT INTO users (name, email, password, role) VALUES ($1, $2, $3, $4)`
-	role := "read"
+	role := "readonly"
 	_, err := c.Exec(context.Background(), query, name, email, password, role)
 	var user User
 	if err != nil {
 		return user, fmt.Errorf("failed to insert user into users table: %v", err)
 	}
-	// ERROR IS BELOW...
 	getNewUserQuery := `SELECT email, name, role FROM users WHERE email = $1`
 	rows, err := c.Query(context.Background(), getNewUserQuery, email)
 	if err != nil {
-		fmt.Println("HIT THE EEARLIER EERRRR")
 		return user, err
 	}
 	defer rows.Close()
-
 	if !rows.Next() {
 		fmt.Println("No rows found for email:", email)
 		return user, fmt.Errorf("no user found with email: %s", email)
 	}
-
-	// Scan the result into the user struct
-err = rows.Scan(&user.Email, &user.Name, &user.Role)
-if err != nil {
-	fmt.Println("Error scanning row:", err)
-	return user, fmt.Errorf("error scanning row: %v", err)
-}
-
-// Check for any errors in rows after the loop
-if rows.Err() != nil {
-	fmt.Println("Error after rows loop:", rows.Err())
-	return user, rows.Err()
-}
-
-return user, nil
-
-	// for rows.Next() {
-	// 	err := rows.Scan(nil, &user.Name, &user.Email, nil, &user.Role)
-	// 	if err != nil {
-	// 		fmt.Println("SCANNING ERROR??")
-	// 		return user, fmt.Errorf("error scanning row %v", err)
-	// 	}
-	// }
-	// if rows.Err() != nil {
-	// 	fmt.Println("ROWS	 ERROR??")
-	// 	return user, rows.Err()
-	// }
-	// return user, nil
+	err = rows.Scan(&user.Email, &user.Name, &user.Role)
+	if err != nil {
+		fmt.Println("Error scanning row:", err)
+		return user, fmt.Errorf("error scanning row: %v", err)
+	}
+	if rows.Err() != nil {
+		fmt.Println("Error after rows loop:", rows.Err())
+		return user, rows.Err()
+	}
+	return user, nil
 }
