ced21360e0e6b95241eb8c1e9ab36c2adb404522
Thomas Harbert <tgharbert3@gmail.com>
2025-01-21 15:43:43 -0600

    further styling

--- internal/domain/document/repository.go
+++ internal/domain/document/repository.go
@@ -12,7 +12,6 @@ type DocumentRepository interface {
 	FindStagedByType(ctx context.Context, orgID uuid.UUID, fileType string) (*Document, error)
 	Delete(ctx context.Context, doc *Document) error
 	GetAllByOrgId(ctx context.Context, orgID uuid.UUID) ([]*Document, error)
-	// GetKeysForDeleteAll(ctx context.Context, orgID int) ([]string, error)
 	GetDocumentDetails(ctx context.Context, docID uuid.UUID) (*Document, error)
 	FindStagedByOrganization(ctx context.Context, orgID uuid.UUID) ([]*Document, error)
 	UpdateDocument(ctx context.Context, doc *Document) error
@@ -26,6 +25,5 @@ type S3Repository interface {
 	UploadFile(ctx context.Context, doc *Document, fileBody interface{}) (string, error)
 	DeleteFile(ctx context.Context, doc *Document) error
 	DeleteAllOrgFiles(ctx context.Context, keys []string) error
-	// yet to write
 	DownloadFile(ctx context.Context, fileName string, ID uuid.UUID) (*s3.GetObjectOutput, error)
 }
